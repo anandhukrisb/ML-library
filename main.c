901d3166b5f03e4376294e027a0cf150a5e4f605
krisb <anandhukrishna10b@gmail.com>
2026-05-25 01:45:29 +0530

    functions added

--- main.c
+++ main.c
@@ -23,3 +23,76 @@ typedef i32 b32;
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #define ALIGN_UP_POW2(n, p) (((u64)(n) + ((u64)(p) - 1)) & (~((u64)(p) - 1)))
+
+#define ARENA_BASE_POS (sizeof(mem_arena))
+#define ARENA_ALIGN (sizeof(void* ))
+
+typedef struct {
+    u64 capacity;
+    u64 pos;
+} mem_arena;
+
+mem_arena* arena_create(u64 capacity);
+void arena_destroy(mem_arena* arena);
+void* arena_push(mem_arena* arena, u64 size, b32 non_zero);
+void arena_pop(mem_arena* arena, u64 size);
+void arena_pop_to(mem_arena* arena, u64 pos);
+void arena_clear(mem_arena* arena);
+
+int main() {
+
+        
+
+    return 0;
+}
+
+mem_arena* arena_create(u64 capacity) {
+
+    mem_arena* = (mem_arena*)malloc(capacity);
+
+    arena -> capacity = capacity;
+    arena -> pos = ARENA_BASE_POS;
+    
+    return arena;
+
+}
+
+void arena_destroy(mem_arena* arena); {
+
+    free(arena);
+
+}
+
+void* arena_push(mem_arena* arena, u64 size, b32 non_zero) {
+
+    u64 pos_aligned = ALIGN_UP_POW2(arena->pos, ARENA_ALIGN);
+    u64 new_pos = pos_aligned + size;
+
+    if(new_pos > arena -> capacity) { return NULL; }
+
+    arena -> pos = new_pos;
+
+    u8* out = (u8*)arena + pos_aligned;
+
+    if(!non_zero) {
+        memset(out, 0, size);
+    }
+
+    return out;
+
+}
+
+void arena_pop(mem_arena* arena, u64 size) {
+    size = MIN(size, arena -> pos - ARENA_BASE_POS);
+    arena -> pos -= size;
+}
+
+void arena_pop_to(mem_arena* arena, u64 pos) {
+    u64 size = pos < arena -> pos ? arena -> pos - pos : 0;
+
+    arena_pop(arena, size);
+}
+
+void arena_clear(mem_arena* arena) {
+    arena_pop_to(arena, ARENA_BASE_POS);
+}
